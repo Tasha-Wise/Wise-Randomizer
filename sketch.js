ea6b615abba55629e216c4d37b957bcb032198fd
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-09-24 18:34:46 -0700

    update

--- sketch.js
+++ sketch.js
@@ -1,20 +1,23 @@
-// Rotate this rectangle around it's center point!
+// dogs I have loved before
+let dogs = [{name:"dillon", color: "dog treat color"}, {name: "Ziggy", color:"chartrese"}, {name; "mercedes", color:"poop"}, {name: "nala", color:"stardust"}, {name; "kloot", color:"coffee grind brown"}, {name; "petunia", color:"rose gold"}];
+
+let randomIndex;
 
 function setup() {
-  createCanvas(400, 400);
-  background(220);
-  rectMode(CENTER);
+	createCanvas(600, 600);
+	background(200);	
 
-  ellipse(50, 150, 30, 60);
-  
-  push();
-  
-  translate(200,110);
-  rotate(PI/6);
-  rect(0, 0, 200, 20);
-  
-  pop();
+	
+	
 }
 
-function draw() {
+function draw(){
+	
+}
+
+function mousePressed(){
+	background(random(200, 255));
+	randomIndex = int(random(dogs.length));
+	text(dogs[randomIndex].name, 50, 50);
+	dogs.splice(randomIndex, 1);
 }
