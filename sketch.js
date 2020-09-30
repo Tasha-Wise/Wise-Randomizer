d168b35255b09a7ead62ca04e3fbded0f1e8e8fe
Tasha-Wise <60452066+Tasha-Wise@users.noreply.github.com>
2020-09-29 17:22:07 -0700

    updated

--- sketch.js
+++ sketch.js
@@ -1,23 +1,70 @@
 // dogs I have loved before
-let dogs = [{name:"Lakota", color: "pink"}, {name: "Kiowa", color:"blue"}, {name:"Druid", color:"red"}, {name: "Skye", color:"frisbee"}, {name: "Shasta", color:"dog treat"}, {name:"Sona", color:"fruit"}];
+let dogs = [{
+  name: "Lakota",
+  color: "pink"
+}, {
+  name: "Kiowa",
+  color: "blue"
+}, {
+  name: "Druid",
+  color: "red"
+}, {
+  name: "Skye",
+  color: "frisbee"
+}, {
+  name: "Shasta",
+  color: "dog treat"
+}, {
+  name: "Sona",
+  color: "fruit"
+}];
 
 let randomIndex;
+let counter = 0
+let animating = false;
 
 function setup() {
-	createCanvas(600, 600);
-	background(200);	
+  createCanvas(600, 600);
+  background(200);
+  textSize(32);
+  text("Click to Randomize!", 50, 50);
 
-	
-	
+
+
+
+
 }
 
-function draw(){
-	
+function draw() {
+
+  if (animating == true) {
+    ellipse(random(width), random(height), random(50, 200));
+  }
+
 }
 
-function mousePressed(){
-	background(random(200, 255));
-	randomIndex = int(random(dogs.length));
-	text(dogs[randomIndex].name, 50, 50);
-	dogs.splice(randomIndex, 1);
+
+
+function randomizer() {
+  animating = false;
+  if (dogs[0]) {
+    background(random(200, 255));
+    randomIndex = int(random(dogs.length));
+    text(`${dogs[randomIndex].name} 's favorite color is
+${dogs[randomIndex].color}`, 50, 50);
+    
+    //text(dogs[randomIndex].name + "'s favorite color is" + dogs[randomIndex].color, 50, 50);
+    dogs.splice(randomIndex, 1);
+
+  } else {
+    background(random(200, 255));
+    text("nothing left!", 50, 50);
+  }
+}
+
+function mousePressed() {
+  animating = true;
+  setTimeout(randomizer, 2000);
+
+
 }
